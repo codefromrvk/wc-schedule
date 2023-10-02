5cd224b734280017ee82d2bf8bc269ef86610352
codefromrvk <varshithkumar553@gmail.com>
2023-10-02 13:37:40 +0530

    fix: remove sound click on mobile

---
+++ src/components/global/Sound.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
+
+const Sound = () => {
+  const [sound, setSound] = useState(false);
+  const handleToggleSound = () => {
+    const currentState = !sound;
+
+    if (currentState) {
+      const clickableBtnList =
+        document.getElementsByClassName("btn-click-audio");
+      for (let btn of clickableBtnList) {
+        btn.classList.remove("btn-click-audio");
+        // removeEventListener("click", () => {
+        // //   audio.play();
+        // });
+      }
+    }
+    setSound(currentState);
+  };
+
+  return (
+    <>
+      {sound ? (
+        <GiSpeaker size={25} onClick={handleToggleSound} />
+      ) : (
+        <GiSpeakerOff size={25} onClick={handleToggleSound} />
+      )}
+    </>
+  );
+};
+
+export default Sound;
