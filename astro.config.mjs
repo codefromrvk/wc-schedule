e9b362f11185ef2a27f830f78db59da3053e2134
codefromrvk <varshithkumar553@gmail.com>
2025-04-27 16:53:01 +0530

    update site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://worldcup.rvkay.in",
+  site: "https://worldcup2023.varshithkumar.com",
   integrations: [tailwind(), sitemap(), react()],
   vite: {
     ssr: {
